a51fb9cdafe1142c867e4d145eb2460873428551
Nicolas HENRI <nicolas.henri@st-groupe.fr>
2014-11-07 10:49:30 +0100

    Initial Commit

---
+++ githubFormation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubFormation
+//
+//  Created by Nicolas HENRI on 07/11/2014.
+//  Copyright (c) 2014 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubFormation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubFormation
+//
+//  Created by Nicolas HENRI on 07/11/2014.
+//  Copyright (c) 2014 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
